68b8a264060ab062d004b82463b46fe881a8de28
Victor King'e <vinge1718@gmail.com>
2017-10-03 01:41:25 +0300

    add the delete notice function in the notice ccomponent

--- app/components/notice-tile.js
+++ app/components/notice-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    delete(notice){
+      if (confirm("Are you sure you want to delete this notice item?")){
+        this.sendAction("deleteNotice", notice);
+      }
+    }
+  }
 });
