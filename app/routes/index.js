0fc01cf5b9d3fbc5a9e65a47811101eddcbec347
Victor King'e <vinge1718@gmail.com>
2017-10-03 05:35:39 +0300

    add the new variables containing the new notice and property properties

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,5 +8,29 @@ import Ember from 'ember';
         notices: this.store.findAll("notice"),
         propertys: this.store.findAll("property")
       });
+    },
+
+    actions: {
+      deleteProperty(property){
+        property.destroyRecord();
+        this.transitionTo("index");
+      },
+
+      deleteNotice(notice){
+        notice.destroyRecord();
+        this.transitionTo("index");
+      },
+
+      saveProperty3(params){
+        var newProperty = this.store.createRecord("property", params);
+        newProperty.save();
+        this.transitionTo("index");
+      },
+
+      saveNotice3(params) {
+        var newNotice = this.store.createRecord("notice", params);
+        newNotice.save();
+        this.transitionTo("index");
+      }
     }
 });
