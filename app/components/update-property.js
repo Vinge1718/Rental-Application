52d6abe23610f40588f472679faae0d11ed95f17
Victor King'e <vinge1718@gmail.com>
2017-10-03 09:54:38 +0300

    add the files necessary to update the properties component

---
+++ app/components/update-property.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updatePropertyForm: false,
+  actions:{
+    updatePropertyForm(){
+      this.set("updatePropertyForm", true)
+    },
+    update(property){
+      var params = {
+        location: this.get("location"),
+        town: this.get("town"),
+        cost: this.get("cost"),
+        bedrooms: this.get("bedrooms"),
+        image: this.get("image")
+      };
+      this.set("updatePropertyForm", false);
+      this.sendAction("update", property, params);
+    }
+  }
+});
